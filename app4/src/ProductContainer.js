7cc860b1defae3020ee4990f3a60165433d0fce5
Toby Smyth <tobs169@gmail.com>
2017-09-29 13:32:21 +0100

    Lookup product deets

--- app4/src/ProductContainer.js
+++ app4/src/ProductContainer.js
@@ -4,9 +4,10 @@ import data from './data.js'
 import './ProductContainer.css'
 import Product from './Product.js'
 
+const lookupProduct = (name) => _.find(data.products, (p => p.name == name))
 
-const ProductContainer = (props) => <div className='product-container'>
-      {props.product}
+const ProductContainer = ({product}) => <div className='product-container'>
+      <Product product={lookupProduct(product)}/>
     </div>
 
 export default ProductContainer
