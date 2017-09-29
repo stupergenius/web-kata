3d0d602bca3f72d7d5277671cc2a654331dc11ec
Toby Smyth <tobs169@gmail.com>
2017-09-29 13:10:02 +0100

    add browser router

--- app4/src/App.js
+++ app4/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import data from './data.js'
+import {BrowserRouter} from 'react-router-dom'
 
 import ProductMenu from './ProductMenu.js'
 import ProductContainer from './ProductContainer.js'
@@ -17,10 +18,12 @@ class App extends Component {
       <div className="App-header">
         <h2>Kata 4- Add router to an app</h2>
       </div>
-      <div className='products-container'>
-        <ProductMenu products={this.state.products} />
-        <ProductContainer />
-      </div>
+      <BrowserRouter>
+        <div className='products-container'>
+          <ProductMenu products={this.state.products} />
+          <ProductContainer />
+        </div>
+      </BrowserRouter>
     </div>
   }
 }
