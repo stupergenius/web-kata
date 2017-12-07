99adacdb2125cce25589e6b3de368328fa348779
Greg Smulko <greg.smulko@red-gate.com>
2017-12-07 15:40:39 +0000

    Add product

--- app5/app/src/App.js
+++ app5/app/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
     super(props)
     this.state = { products: [] }
     this.removeProduct = this.removeProduct.bind(this);
+    this.addProduct = this.addProduct.bind(this);
     this.fetchProducts()
   }
 
@@ -47,12 +48,43 @@ class App extends Component {
     }.bind(this))
   }
 
+  addProduct(event){
+    event.preventDefault()
+    
+    const name = event.target.productName.value
+    const description = event.target.productDescription.value
+    const data = { name, description }
+    
+    const url = '/api/products/add'
+    const options = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      },
+    }
+
+    fetch(url, options)
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(json) {
+      this.setState({ products: json })
+    }.bind(this))
+  }
+
   render() {
     return <div className="App">
       <div className="App-header">
         <h2>Kata 5 - Interaction with backend server through REST API calls</h2>
       </div>
-      <div className='products-add-product'>add product here</div>
+      <div className='products-add-product'>
+        <form onSubmit={this.addProduct}>
+          <label>name: <input name="productName" /></label>
+          <label>description: <input name="productDescription" /></label>
+          <input type="submit" />
+        </form>
+      </div>
       <div className='products-container'>
         <ProductMenu products={this.state.products} removeHandler={this.removeProduct} />
         <Route exact path='/products/:productName' component={
