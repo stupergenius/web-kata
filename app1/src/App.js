577ec3f548ed61206cfc3552c89f1669106aac55
Benjamin Snider <stupergenius@gmail.com>
2018-01-18 03:29:53 +0000

    Finishing kata1

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react'
+import data from './data.js'
 import './App.css'
+import Products from './Products'
 
 class App extends Component {
   render() {
     return <div className="App">
       <div className="App-header">
-        <h2>Welcome Introduction to <code>web-kata#1</code></h2>
+        <h2>Welcome Introduction to <code>web-kata numero uno</code></h2>
       </div>
-      <p className="App-intro">
-        To get started change this text and save save to reload.
-      </p>
-      <div className='products'>
-        ... your code here
-      </div>
+      <Products products={data.products} />
     </div>
   }
 }
