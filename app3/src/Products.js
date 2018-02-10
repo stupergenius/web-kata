13f6dfe58df95d3224b523991d13a4497a112c3d
Benjamin Snider <stupergenius@gmail.com>
2018-02-10 21:14:28 +0000

    Finishing kata3. Adds toggle capability to product description.

--- app3/src/Products.js
+++ app3/src/Products.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react'
 import './Products.css'
 
-class Product extends Component{
-    render(){
+class Product extends Component {
+    constructor(props){
+      super(props)
+      this.state = {showDescription: false}
+      
+      this.handleToggle = this.handleToggle.bind(this)
+    }
+    
+    handleToggle() {
+      this.setState({showDescription: !this.state.showDescription})
+    }
+    
+    render() {
         return <div className='product'>
             <div className='details'>
                 <div className='name'>{this.props.product.name}</div>
-                <div className='desc'>{this.props.product.description}</div>
+                {this.state.showDescription &&
+                  <div className='desc'>{this.props.product.description}</div>}
             </div>
             <div className='actions'>
                 <div className='remove' title='fix me' onClick={() => this.props.removeProduct(this.props.product)}>x</div>
+                <div className='toggleDescription'
+                  title='fix me'
+                  onClick={this.handleToggle}>
+                  {this.state.showDescription ? '-' : '+'}
+                </div>
             </div>
         </div>
     }
 }
 
-class Products extends Component{
+class Products extends Component {
     render(){
         return <div className='products'>
             {this.props.products.map(
-                (p, i) => 
+                (p, i) =>
                 <Product
                     product={p}
                     key={'product-' + i }
