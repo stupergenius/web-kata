452925eaf794ac01efbbdddd37e324873052a32b
Benjamin Snider <stupergenius@gmail.com>
2018-02-11 00:20:47 +0000

    Adding support for creating a product over the API.

--- app5/app/src/App.js
+++ app5/app/src/App.js
@@ -21,7 +21,34 @@ class App extends Component {
       this.setState({error: true});
     });
     
+    this.handleAdd = this.handleAdd.bind(this)
     this.handleDelete = this.handleDelete.bind(this)
+  }
+  
+  handleAdd(event) {
+    event.preventDefault()
+
+    const newProduct = {
+      name: event.target.name.value,
+      description: event.target.description.value
+    }
+    
+    const options = {
+      method: "POST",
+      body: JSON.stringify(newProduct),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    fetch("http://159.89.147.145:5000/api/products/add", options).then(response => {
+      response.json().then(products => {
+        this.setState({products: products})
+      }, error => {
+        this.setState({error: true});
+      })
+    }, error => {
+      this.setState({error: true});
+    });
   }
   
   handleDelete(product) {
@@ -42,7 +69,17 @@ class App extends Component {
       <div className="App-header">
         <h2>Kata 5 - Interaction with backend server through REST API calls</h2>
       </div>
-      <div className='products-add-product'>add product here</div>
+      <div className='add-product'>
+        <form onSubmit={this.handleAdd}>
+          <label>product name:
+            <input type='text' name='name' />
+          </label>
+          <label>description:
+            <input type='text' name='description'/>
+          </label>
+          <input type='submit' value='add product' />
+        </form>
+      </div>
       <div className='products-container'>
         <ProductMenu handleDeleteProduct={this.handleDelete} products={this.state.products} />
         <Route exact path='/products/:productName' component={
